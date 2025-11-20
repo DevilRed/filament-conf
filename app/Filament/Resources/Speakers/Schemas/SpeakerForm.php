be589b4800a0bee4121fcf4c2e7e3597b93cce57
Christian Quispe Rojas <cquispe@develoop.net>
2025-11-20 08:42:05 -0400

    show checklist input on speaker form for qualifications

--- app/Filament/Resources/Speakers/Schemas/SpeakerForm.php
+++ app/Filament/Resources/Speakers/Schemas/SpeakerForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Speakers\Schemas;
 
+use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
@@ -23,6 +24,26 @@ class SpeakerForm
                     ->columnSpanFull(),
                 TextInput::make('twitter_handle')
                     ->required(),
+            CheckboxList::make('qualifications')
+                ->columnSpanFull()
+                ->searchable()
+                ->options([
+                    'business-leader' => 'Business Leader',
+                    'charisma' => 'Charismatic Speaker',
+                    'first-time' => 'First Time Speaker',
+                    'hometown-hero' => 'Hometown Hero',
+                    'humanitarian' => 'Works in Humanitarian Field',
+                    'laracast-contributor' => 'Laracast Contributor',
+                    'twitter-influencer' => 'Large Twitter Following',
+                    'youtube-influencer' => 'Large YouTube Following',
+                    'open-source' => 'Open Source Creator / Maintainer',
+                    'unique-perspective' => 'Unique Perspective',
+                ])
+                ->descriptions([ // just as example
+                    'business-leader' => 'Here is a nice long description',
+                    'charisma' => 'This is even more information about why you should pick this one',
+                ])
+                ->columns(3)
             ]);
     }
 }
