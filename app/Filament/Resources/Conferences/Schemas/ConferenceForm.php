de7abfe60a3bf0b28f9d9b2110126fccb86760c2
Christian Quispe Rojas <cquispe@develoop.net>
2025-11-20 10:08:31 -0400

    put clarifications

--- app/Filament/Resources/Conferences/Schemas/ConferenceForm.php
+++ app/Filament/Resources/Conferences/Schemas/ConferenceForm.php
@@ -3,16 +3,20 @@
 namespace App\Filament\Resources\Conferences\Schemas;
 
 use App\Enums\Region;
+use App\Filament\Resources\Speakers\SpeakerResource;
 use App\Filament\Resources\Venues\Schemas\VenueForm;
+use App\Models\Speaker;
 use Filament\Schemas\Components\Utilities\Get;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\RichEditor;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ConferenceForm
 {
@@ -43,19 +47,30 @@ class ConferenceForm
                         'archived' => 'Archived',
                     ]),
                 Select::make('region')
-                    // dependent select with livewire
-                    ->live()// make a request to the server to refresh section
-                    ->enum(Region::class)// to validate only enum vals are allowed
-                    ->options(Region::class),
-                Select::make('venue_id')
-                    ->searchable()// search box
-                    ->preload()// list values
-                    ->createOptionForm(VenueForm::getForm())// show + icon to add a venue
-                    ->editOptionForm(VenueForm::getForm())
-                    ->relationship('venue', 'name', function (Builder $query, Get $get) {
-                        return $query->where('region', $get('region'));
-                    })
-                    ->default(null),
+                // this works as a parent select, so when the user selects a region, the venue select field is refreshed
+                ->live() // enable livewire, any change to this select, make a request to the server and the whole component is going to be re-rendered, so the closure in venue field is going to run again
+
+                ->enum(Region::class) // to validate only enum vals are allowed
+                ->options(Region::class), // get options from enum
+
+            Select::make('venue_id') // this is the child field, a conference has many venues
+                ->searchable() // search box
+                ->preload() // preload list values, so the search is faster, recommended for small lists
+
+                ->createOptionForm(VenueForm::getForm()) // show + icon to add a venue, clicking on it will open the create form modal
+
+                ->editOptionForm(VenueForm::getForm()) // pass the form to the edit action, once the value is selected, an edit icon will appear, clicking on it will open the edit form modal
+
+                ->relationship(name: 'venue', titleAttribute: 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+                    // make venue dependent on region field value using $get to get the value of the region field
+                    return $query->where('region', $get('region'));
+                }),
+            CheckboxList::make('speakers')
+                ->relationship('speakers', 'name')
+                ->options(
+                    Speaker::all()->pluck('name', 'id'),
+                )
+                ->required(),
             ]);
     }
 }
